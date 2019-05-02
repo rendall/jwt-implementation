ff58ff537c7db2392f1ddd7e19384fdcb360ae9b
Rendall <rendall@gmail.com>
2019-05-02 19:12:47 +0300

    Add jest tests of auth

--- src/functions-build/auth.ts
+++ src/functions-build/auth.ts
@@ -3,7 +3,10 @@ import * as jwt from 'jsonwebtoken'
 import * as dotenv from 'dotenv'
 import * as crypto from 'crypto'
 
-dotenv.config()
+const AUTHORIZATION = 'Authorization'
+
+dotenv.config() 
+// dotenv.config() extracts secret constants from .env file as fields of the global `process.env` object
 
 
 export const handler: Handler = ( event: Event, context: Context, callback: Callback) => {
@@ -13,24 +16,22 @@ export const handler: Handler = ( event: Event, context: Context, callback: Call
   }
 };
 
-const decodeAuthHeader = (authHeaderValue) => Buffer.from(authHeaderValue.slice(6), 'base64').toString()
+export const decodeAuthHeader = (authHeaderValue) => Buffer.from(authHeaderValue.slice(6), 'base64').toString()
 /** decodeAuthHeader expects a string of the form `Basic someBase64String==` and returns its plaintext decode */
-const parseAuthHeader = (plainText:string, colonIndex?:number):{user:string, password:string} => colonIndex === undefined? parseAuthHeader(plainText, plainText.indexOf(':')) : ({user:plainText.slice(0,colonIndex),password:plainText.slice(colonIndex+1)}) 
+export const parseAuthHeader = (plainText:string, colonIndex?:number):{user:string, password:string} => colonIndex === undefined? parseAuthHeader(plainText, plainText.indexOf(':')) : ({user:plainText.slice(0,colonIndex),password:plainText.slice(colonIndex+1)}) 
 /** parseAuthHeader expects a string of the form `user:password` and returns an object of the form {user, password} */
-const extractUser = (authHeaderValue) => [decodeAuthHeader, parseAuthHeader].reduce((acc,func) =>func(acc), authHeaderValue)['user']
+export const extractUser = (authHeaderValue) => [decodeAuthHeader, parseAuthHeader].reduce((acc,func) =>func(acc), authHeaderValue)['user']
 /** extractUser expects a string of the form `Basic someBase64String==` and returns the user name */
 const authenticateUser = ({ user, password }): boolean => user === "abc" && hash(password) === process.env.ABC_USER_HASHED_PASSWORD; 
-/** authenticateUser expects an object {user, password} and returns true if the user exists and the password matches the user */
-const isUserAuthentic = (authHeaderValue) => [decodeAuthHeader, parseAuthHeader, authenticateUser].reduce((acc,func) => func(acc), authHeaderValue)
+/** authenticateUser expects an object {user, password} and returns true if the user exists and the password matches the user. In this case, the only valid user is 'abc' with password '123' */
+export const isUserAuthentic = (authHeaderValue) => [decodeAuthHeader, parseAuthHeader, authenticateUser].reduce((acc,func) => func(acc), authHeaderValue)
 /** isUserAuthentic expects authHeaderValue and returns true if the user exists and the password matches the user */
 
 const handleGet = (event:Event, callback:Callback) => {
-  const AUTHORIZATION = 'Authorization'
 
-  // TODO: hasBasicAuth will false positive *any* header, not just those with 'Basic'
-  const hasBasicAuth = hasHeader(event.headers, AUTHORIZATION)
+  const isBasicAuth = hasBasicAuth(event.headers) 
 
-  if (!hasBasicAuth) callback(null, buildResponse( `Unauthenticated`, 401, {...HEADERS, ...AUTHENTICATE_HEADER}))
+  if (!isBasicAuth) callback(null, buildResponse( `Unauthenticated`, 401, {...HEADERS, ...AUTHENTICATE_HEADER}))
   else {
     const authHeaderValue = getHeaderValue(event.headers, AUTHORIZATION)
     const isAuthentic = isUserAuthentic(authHeaderValue)
@@ -48,6 +49,9 @@ const handleGet = (event:Event, callback:Callback) => {
 const hasHeader = (headers:{[header:string]:string}, header:string) => Object.keys(headers).some(h => h.toLowerCase() === header.toLowerCase() && headers[h] !== undefined);
 const getHeader = (headers:{[header:string]:string}, header:string) => Object.keys(headers).find(h => h.toLowerCase() === header.toLowerCase())
 const getHeaderValue = (headers:{[header:string]:string}, header:string) => headers[getHeader(headers, header)] 
+const parseAuthHeaderValue = (authHeaderValue:string, spaceIndex?:number):{scheme:string, credentials:string} => spaceIndex === undefined? parseAuthHeaderValue(authHeaderValue, authHeaderValue.indexOf(' ')):({scheme:authHeaderValue.slice(0, spaceIndex), credentials:authHeaderValue.slice(spaceIndex + 1)})
+const hasBasicAuth = (headers:{[header:string]:string}, parse?:{scheme:string, credentials:string}) => parse === undefined? hasHeader(headers, AUTHORIZATION)? hasBasicAuth(headers, parseAuthHeaderValue(getHeaderValue(headers, AUTHORIZATION))) : false : parse.scheme.toLowerCase() === 'basic'
+
 const buildResponse = ( message: string, statusCode: number = 200, headers = HEADERS) => ({ statusCode, headers, body: JSON.stringify({ message }) });
 
 const HEADERS = {
