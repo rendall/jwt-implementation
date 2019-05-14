084e3d29b58d6fff292fb1b06082b5ec6051471f
Rendall <rendall@gmail.com>
2019-05-14 09:46:08 +0300

    Add expiration rejection test

--- src/functions-build/verify.test.ts
+++ src/functions-build/verify.test.ts
@@ -1,33 +1,47 @@
-import * as http from "http";
+import * as http from "http"
 import * as dotenv from 'dotenv'
+import * as jwt from 'jsonwebtoken'
 
-const VERIFY_ENDPOINT = "http://localhost:9000/.netlify/functions/verify";
+const VERIFY_ENDPOINT = "http://localhost:9000/.netlify/functions/verify"
 
 dotenv.config() 
 const getVerify = () =>
     new Promise<http.IncomingMessage>((resolve, reject) => {
-      http.get(VERIFY_ENDPOINT, res => resolve(res));
-    });
+      http.get(VERIFY_ENDPOINT, res => resolve(res))
+    })
 
 
-const sendAuth = () =>
+const sendAuth = ( token:string = process.env.TEST_USER_BEARER_TOKEN) =>
     new Promise<http.IncomingMessage>((resolve, reject) => {
       const options: http.RequestOptions = {
         headers: {
-          Authorization: `Bearer ${process.env.TEST_USER_BEARER_TOKEN}`
+          Authorization: `Bearer ${token}`
         }
-      };
+      }
 
-      http.get(VERIFY_ENDPOINT, options, res => resolve(res));
-    });
+      http.get(VERIFY_ENDPOINT, options, resolve)
+    })
 
 describe("'verify' API endpoint", () => {
-    test(`'verify' endpoint is served at ${VERIFY_ENDPOINT}`, done =>
-    getVerify().then(() => done()));
+    test(`'verify' endpoint ${VERIFY_ENDPOINT} with no Bearer returns 401`, done =>
+      getVerify().then((res) => {
+        expect(res.statusCode).toBe(401)
+        done();
+      }));
 
-  test(`'verify' with token returns 200`, done =>
+  test(`'verify' with valid token returns 200`, done =>
     sendAuth().then((res) => {
       expect(res.statusCode).toBe(200)
-      done();
-    }));
-});
+      done()
+    }))
+
+  test(`'verify' rejects an expired token`, done => {
+      const token = jwt.sign({user:"user", exp:(new Date(0).valueOf())}, process.env.JWT_SECRET)
+
+      sendAuth(token).then((res) => {
+        expect(res.statusCode).toBe(403)
+        done()
+      })
+
+  })
+})
