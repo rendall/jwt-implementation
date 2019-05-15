538e08090967d7b306c161df9845c981ad5cbb76
Rendall <rendall@gmail.com>
2019-05-15 19:00:23 +0300

    Add error and status

--- src/public/js/client.ts
+++ src/public/js/client.ts
@@ -1,62 +1,105 @@
-const AUTH_ENDPOINT = "/.netlify/functions/auth"
-const VERIFY_ENDPOINT = "/.netlify/functions/verify"
-document.querySelector("form").addEventListener("submit", (submitEvent:Event) => submitEvent.preventDefault())
-document.querySelector("#userSubmitButton").addEventListener("click", (event:Event) => {
-  event.preventDefault()
-  const inputs = Array.from( document.querySelectorAll("form input")).map((i:HTMLInputElement) => [ i.name, i.value ])
-  const user = inputs.find( i => i[0] === "user")[1]
-  const password = inputs.find(i => i[0] === "password")[1]
+const AUTH_ENDPOINT = "/.netlify/functions/auth";
+const VERIFY_ENDPOINT = "/.netlify/functions/verify";
+const credentials: RequestCredentials = "include";
 
-  const encode = `${user}:${password}`
-  const basicCred = window.btoa(encode) 
+const onBasicSubmit = (event: Event) => {
+  event.preventDefault();
+  displayError("")
+  displayStatus("")
+  const inputs = Array.from(
+    document.querySelectorAll("form#userForm input")
+  ).map((i: HTMLInputElement) => [i.name, i.value]);
+  const user = inputs.find(i => i[0] === "user")[1];
+  const password = inputs.find(i => i[0] === "password")[1];
 
-  const credentials:RequestCredentials = 'include'
+  const encode = `${user}:${password}`;
+  const basicCred = window.btoa(encode);
 
   const authReqInfo = {
-    credentials:credentials,
+    credentials: credentials,
     headers: {
       Authorization: `Basic ${basicCred}`
     }
-  }
+  };
 
   XFetch(AUTH_ENDPOINT, authReqInfo)
-    .then( response => response.text().then(text => ({ status:response.status, text:text })))
-    .then(
-      ({status, text}) => { 
-        if (status !== 200) throw `${status}:${text}`;
-        else return text;
-      }
+    .then(response =>
+      response.text().then(text => ({ status: response.status, text: text }))
     )
-    .then(token => {
-      const verifyReqInfo = {
-        credentials: credentials,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      };
-
-      XFetch(VERIFY_ENDPOINT, verifyReqInfo)
-        .then(tap("receiving verification"))
-        .then(
-          response => response.text(),
-          (error: Error | string) => console.error(error)
-        )
-        .then(text => console.log("verified claims", text));
+    .then(({ status, text }) => {
+      if (status !== 200) throw `${status}:${text}`;
+      else return text;
     })
-    .catch(reason => console.error(formatReason(reason)));
-})
+    .then(token => {
+      const input: HTMLInputElement = document.querySelector(
+        "input[name=token]"
+      );
+      input.value = token;
+      displayStatus("The auth API returns a token")
+    })
+    .catch(reason => {
+      displayError(formatReason(reason));
+    });
+};
+
+const displayError = (error: string) => (document.querySelector("#errorDisplay") as HTMLParagraphElement).innerText = error;
+const displayStatus = (status: string) => (document.querySelector("#statusDisplay") as HTMLParagraphElement).innerText = status;
+
+
+const onBearerSubmit = (event: Event) => {
+  event.preventDefault();
+  displayError("")
+  displayStatus("")
+
+  const token = (document.querySelector(
+    "input[name=token]"
+  ) as HTMLInputElement).value;
+
+  const verifyReqInfo = {
+    credentials: credentials,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
+
+  XFetch(VERIFY_ENDPOINT, verifyReqInfo)
+    .then(response =>
+      response.text().then(text => ({ status: response.status, text: text }))
+    )
+    .then(({ status, text }) => {
+      if (status !== 200) throw `${status}:${text}`;
+      else displayStatus(`The API can be certain that: ${text}`) 
+    }).catch(reason => {
+      displayError(formatReason(reason));
+    });
+};
+document
+  .querySelector("form")
+  .addEventListener("submit", (submitEvent: Event) =>
+    submitEvent.preventDefault()
+  );
+document
+  .querySelector("#userSubmitButton")
+  .addEventListener("click", onBasicSubmit);
+document
+  .querySelector("#tokenSubmitButton")
+  .addEventListener("click", onBearerSubmit);
 
 /** Fetch with timeout */
-const XFetch = (input:RequestInfo, init?:XRequestInit ):Promise<Response> => {
-  const DEFAULT_TIMEOUT = 5000
-  const timeout = !!init && !!init.timeout? init.timeout : DEFAULT_TIMEOUT
-  if (!!init && init.signal) throw new Error("Add 'controller' instead of 'signal' to init in XFetch.");
-  const controller = !!init && !!init.controller? init.controller : new AbortController()
+const XFetch = (input: RequestInfo, init?: XRequestInit): Promise<Response> => {
+  const DEFAULT_TIMEOUT = 5000;
+  const timeout = !!init && !!init.timeout ? init.timeout : DEFAULT_TIMEOUT;
+  if (!!init && init.signal)
+    throw new Error("Add 'controller' instead of 'signal' to init in XFetch.");
+  const controller =
+    !!init && !!init.controller ? init.controller : new AbortController();
   const { signal } = controller;
 
   return new Promise((resolve, reject) => {
     const timer = setTimeout(() => {
-      reject(new Error(`Server request timed out after ${timeout/1000} seconds`));
+      reject(
+        new Error(`Server request timed out after ${timeout / 1000} seconds`)
+      );
       controller.abort();
     }, timeout);
 
@@ -67,14 +110,14 @@ const XFetch = (input:RequestInfo, init?:XRequestInit ):Promise<Response> => {
 };
 
 interface XRequestInit extends RequestInit {
-  timeout?:number,
-  controller?:AbortController
+  timeout?: number;
+  controller?: AbortController;
 }
 
 const formatReason = (error: Error | string) =>
   typeof error === "string" ? error : `${error.name}:${error.message}`;
 
 const tap = message => arg => {
-  console.log(message, arg)
-  return arg
-}
+  console.log(message, arg);
+  return arg;
+};
